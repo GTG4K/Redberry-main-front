a84996c77e3aacd484c455885cd70828bd3b72a6
giorgi tarkhnishvili <giorgi.tarkhnishvili@redberry.ge>
2023-06-16 23:09:31 +0400

    feat: important commit

--- src/config/vee-validate/rules.js
+++ src/config/vee-validate/rules.js
@@ -1,9 +1,9 @@
-import { defineRule } from "vee-validate"; // npm install vee-validate --save
+import {defineRule} from "vee-validate"; // npm install vee-validate --save
 import {required, email, min, numeric, max, confirmed} from "@vee-validate/rules"; // npm install @vee-validate/rules
 
+defineRule('confirmed', confirmed)
 defineRule("required", required);
 defineRule("numeric", numeric);
 defineRule("email", email);
 defineRule("min", min);
 defineRule("max", max);
-defineRule('confirmed',confirmed)
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,6 @@
 </template>
 
 <script setup>
-import LandingView from "./LandingView.vue";
 import HeaderComponent from "../components/HeaderComponent.vue";
 </script>
 
